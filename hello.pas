556cc6f849f0bfb70256fb649a15ed5691601769
Денис <rypunov2005@bk.ru>
2023-10-04 21:18:37 +0300

    Задание выполнено

--- hello.pas
+++ hello.pas
@@ -1,2 +1,17 @@
 ﻿##
-print('Привет, мир!');
+var lang := ReadString('На каком языке вы говорите? Ответьте в именительном падеже:');
+if lang = 'Русский' then
+  print('Привет');
+if (lang = 'Английский') or (lang = 'English') then
+  print('Hello');
+if (lang = 'Немецкий') or (lang = 'Deutsch') then
+  print('Hallo');
+if (lang = 'Китайский') or (lang = '中國人') then
+  print('你好');
+if (lang = 'Французский') or (lang = 'Français') then
+  print('Bonjour');
+{log1: На каком языке вы говорите? Ответьте в именительном падеже: Китайский
+你好 
+log2: На каком языке вы говорите? Ответьте в именительном падеже: Русский
+Привет 
+log3: Deutsch}
